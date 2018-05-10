ad5b896e44c687d47d9d419c3faee59d16dd29aa
Marcus Cheung <myc2131@tc.columbia.edu>
2018-05-10 16:42:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, newName){
+  katzDeliLine.push(newName);
+  return ('Welcome, ${newName}, You are number ${katzDeliLine.length} in line.');
+}
+
+function nowServing(katzDeli){
+  let i = 0;
+  while (i < katzDeli.length){
+    i++;
+  }
+  if (katzDeli.length === 0){
+    return 'There is nobody waiting to be served!';
+  }
+  else{
+    return 'Currently serving ${katzDeli.shift()}';
+  }
+}
+
+var line = [];
+
+function currentLine(line){
+  for (var i = 0; i < line.length; i++){
+    line.push('[1 + i]' + '. ' + 'line[i]');
+  }
+  if (line.length === 0){
+    return 'The line is currently empty.';
+  }
+  else{
+    return('The line is currently: ' + line);
+  }
+}
